5487b95eba388949d926e08047ba78e1503c94e4
Vinh <v.vinhlt6@vinid.net>
2019-10-30 11:46:05 +0700

    Add IntrinsicHeightWidgetParser

---
+++ lib/dynamic_widget/basic/intrinsic_height_widget_parser.dart
@@ -0,0 +1,18 @@
+import 'package:dynamic_widget/dynamic_widget.dart';
+import 'package:flutter/widgets.dart';
+
+class IntrinsicHeightWidgetParser extends WidgetParser {
+  @override
+  bool forWidget(String widgetName) {
+    return "IntrinsicHeight" == widgetName;
+  }
+
+  @override
+  Widget parse(Map<String, dynamic> map, BuildContext buildContext,
+      ClickListener listener) {
+    return IntrinsicHeight(
+      child: DynamicWidgetBuilder.buildFromMap(
+          map["child"], buildContext, listener),
+    );
+  }
+}
